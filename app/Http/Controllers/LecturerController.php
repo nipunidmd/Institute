454665c9e13e8f0310f65157e39bbac53e4c2c2a
aloka18 <alokawickramage@gmail.com>
2018-09-05 20:37:08 +0530

    my firt commit

---
+++ app/Http/Controllers/LecturerController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\Lecturer;
+
+/**
+ * 
+ */
+class LecturerController extends Controller
+{
+	
+	/**
+     * Show the profile for the given user.
+     *
+     * @param  null
+     * @return Response
+     */
+	public function lecturerAdd()
+	{
+		# code...
+	}
+
+
+
+	/**
+     * Show the profile for the given user.
+     *
+     * @param  null
+     * @return Response
+     */
+	public function lecturerSave()
+	{
+		# code...
+	}
+
+
+	/**
+     * Show the profile for the given user.
+     *
+     * @param  null
+     * @return Response
+     */
+	public function lecturerViewHome()
+
+	{
+		# code...
+	}
+	
+
+
+/**
+     * Show the profile for the given user.
+     *
+     * @param  null
+     * @return Response
+     */
+	public function lecturerList()
+
+	{
+		# code...
+	}
+
+
+	/**
+     * Show the profile for the given user.
+     *
+     * @param  null
+     * @return Response
+     */
+	public function lecturerView()
+
+	{
+		# code...
+	}
+
+
+	/**
+     * Show the profile for the given user.
+     *
+     * @param  null
+     * @return Response
+     */
+	public function lecturerSearch()
+
+	{
+		# code...
+	}
+
+
+/**
+     * Show the profile for the given user.
+     *
+     * @param  null
+     * @return Response
+     */
+	public function lecturerUpdate()
+
+	{
+		# code...
+	}
+
+
+
+	/**
+     * Show the profile for the given user.
+     *
+     * @param  null
+     * @return Response
+     */
+	public function lecturerDelete()
+
+	{
+		# code...
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,22 @@
 |
 */
 
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 Route::get('admin/master','AdminController@view');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('lecturer/lecturer_add','LecturerController@lecturerAdd');
+Route::post('lecturer/lecturer_add','LecturerController@lecturerSave');
+Route::get('lecturer/lecturer_home','LecturerController@lecturerViewHome');
+Route::get('lecturer/lecturer_list','LecturerController@lecturerList');
+Route::get('lecturer/lecturer_view','LecturerController@lecturerView');
+Route::get('lecturer/lecturer_view','LecturerController@lecturerSearch');
+Route::get('lecturer/lecturer_update','LecturerController@lecturerUpdate');
+Route::get('lecturer/lecturer_delete','LecturerController@lecturerDelete');
